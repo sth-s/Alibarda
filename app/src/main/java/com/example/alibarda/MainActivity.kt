ac9c2bc4f21955a901544bba8872061985ed2565
Stefan Shushpanov <stefan.shushpanov@gmail.com>
2022-11-09 17:56:25 +0100

    hellow World!

--- app/src/main/java/com/example/alibarda/MainActivity.kt
+++ app/src/main/java/com/example/alibarda/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.alibarda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var emptyText: TextView
+    private lateinit var hi: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        emptyText = findViewById(R.id.emptyText)
+        hi = findViewById(R.id.hi)
+        hi.setOnClickListener {
+            emptyText.text = "Hello World!"
+        }
     }
 }
